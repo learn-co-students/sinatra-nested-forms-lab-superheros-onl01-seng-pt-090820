66351fca8022e97d7d20d2558dff860d74ccd130
Erick Castille <erick@Ericks-MacBook-Air.local>
2020-12-07 20:52:30 -0500

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,21 @@
+class SuperHero
+    attr_accessor :name, :power, :bio 
+
+    SUPERHEROS = []
+
+    def initialize (params)
+        @name = params[:name]
+        @power = params[:power]
+        @bio = params[:bio]
+        SUPERHEROS << self
+    end
+
+    def save 
+        self.class.all << self
+    end
+    
+    def self.all 
+        SUPERHEROS 
+    end
+
+end
